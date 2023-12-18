4f86e99297402cfec31550048de5e00a3e567509
Matt <59450879+mattppal@users.noreply.github.com>
2023-12-18 14:14:58 -0800

    solutions

--- magic-zoomcamp/data_loaders/load_customers.py
+++ magic-zoomcamp/data_loaders/load_customers.py
@@ -13,13 +13,20 @@ def load_data_from_api(*args, **kwargs):
     Template for loading data from API
     """
     url = 'https://github.com/datablist/sample-csv-files/raw/main/files/customers/customers-500000.zip?raw=True'
-
+    
     return pd.read_csv(url, sep=',', compression='zip')
+
+@test
+def test_rows(output, *args) -> None:
+    """
+    Check num rows in response
+    """
+    assert len(output) == 500000 , 'There are a different number of rows than expected!'
 
 
 @test
-def test_output(output, *args) -> None:
+def test_uniqueness(output, *args) -> None:
     """
-    Template code for testing the output of the block.
+    Test all Id's are unique
     """
-    assert output is not None, 'The output is undefined'
+    assert True not in output['Customer Id'].duplicated().unique() , 'There are duplicate Ids!'
--- magic-zoomcamp/transformers/clean_phone.py
+++ magic-zoomcamp/transformers/clean_phone.py
@@ -8,14 +8,16 @@ if 'test' not in globals():
 
 @transformer
 def transform(data, *args, **kwargs):
+    data.drop(columns=["Index"], inplace=True)
+
     data.columns = (data.columns
                 .str.replace(' ', '_', regex=True)
                 .str.lower()
              )
 
-    data['connection_method'] = data.apply(lambda row: row['website'].split('://')[0], axis=1)
+    data['protocol'] = data.apply(lambda row: row['website'].split('://')[0], axis=1)
 
-    data = data[data['connection_method'] == 'https']
+    data = data[data['protocol'] == 'https']
     
     return data
 
